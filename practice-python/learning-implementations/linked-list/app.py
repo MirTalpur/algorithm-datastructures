ea036a3d2a325c0d443f75112331cafba433d9b8
MirTalpur <hskalee123@gmail.com>
2017-10-01 22:39:33 -0700

    Add linked list implementation

---
+++ practice-python/learning-implementations/linked-list/app.py
@@ -0,0 +1,12 @@
+from linked_list import LinkedList
+
+linkedList = LinkedList()
+
+linkedList.insertStart(1)
+linkedList.insertStart(2)
+linkedList.insertStart(10)
+# linkedList.traverseList()
+linkedList.remove(2)
+# linkedList.traverseList()
+
+print(linkedList.size())
