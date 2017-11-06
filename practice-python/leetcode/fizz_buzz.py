176fc05f39df7d440b923d08e45f73a410844d5f
Mir Ali Talpur <hskalee123@gmail.com>
2017-11-05 16:12:09 -0800

    Implement fizzbuzz with list

--- practice-python/leetcode/fizz_buzz.py
+++ practice-python/leetcode/fizz_buzz.py
@@ -4,14 +4,18 @@
 # five output Buzz. For numbers which are multiples 
 # of both three and five output FizzBuzz.
 def fizz_buzz(n):
+	fizz_buzz_list = []
 	for i in range(1, n + 1):
 		if i % 3 == 0 and i % 5 == 0:
+			fizz_buzz_list.append("FizzBuzz")
 			print("FizzBuzz")
 		elif i % 5 == 0:
-			print("Buzz")
+			fizz_buzz_list.append("Buzz")
 		elif i % 3 == 0:
-			print("Fizz")
+			fizz_buzz_list.append("Fizz")
 		else:
-			print(str(i))
+			fizz_buzz_list.append(str(i))
+
+	print fizz_buzz_list
 
 fizz_buzz(15)
