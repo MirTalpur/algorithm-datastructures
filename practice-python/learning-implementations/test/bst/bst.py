31abea45b28fa6f5331c38b5f0076db797af3816
Mir Ali Talpur <hskalee123@gmail.com>
2017-11-07 11:13:10 -0800

    Add remove to bst implementation in test

--- practice-python/learning-implementations/test/bst/bst.py
+++ practice-python/learning-implementations/test/bst/bst.py
@@ -5,6 +5,45 @@ class BST(object):
   def __init__(self):
     self.root = None
 
+  def getMin(self, parent_node):
+    if parent_node.left is None:
+      return parent_node.data
+    else:
+      return  self.getMin(parent_node.left)
+
+  def _remove(self, value, parent_node, temp_node):
+    if value < parent_node.data:
+      if parent_node.left is not None:
+        self._remove(value, parent_node.left, parent_node)
+    elif value > parent_node.data:
+      if parent_node.right is not None:
+        self._remove(value, parent_node.right, parent_node)
+    else:
+      if parent_node.left is not None and parent_node.right is not None:
+        parent_node.data = self.getMin(parent_node.right)
+        self._remove(parent_node.data, parent_node.right, parent_node)
+      elif temp_node.left == parent_node:
+        if parent_node.left is not None:
+          temp = parent_node.left
+        else:
+          temp = parent_node.right
+        temp_node.left = temp
+      elif temp_node.right == parent_node:
+        if parent_node.left is not None:
+          temp = parent_node.left
+        else:
+          temp = parent_node.right
+        temp_node.right = temp
+
+  def remove(self, value):
+    if self.root is not None:
+      if self.root.data == value:
+        temp_node = Node(None)
+        temp_node.left = self.root
+        self._remove(value, self.root, temp_node)
+      else:
+        self._remove(value, self.root, None)
+
   def _insert(self, value, parent_node):
     if value < parent_node.data:
       if parent_node.left is None:
@@ -38,5 +77,6 @@ class BST(object):
 
 bst = BST()
 bst.insert(2)
-bst.insert(1)
+bst.insert(3)
+bst.remove(3)
 bst.traverse_tree()
