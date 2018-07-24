76b8cfafa5303110a3b539c16c00144b7ab728df
Mir Ali Talpur <hskalee123@gmail.com>
2018-07-23 23:59:06 -0700

    Re-arrange project structure

--- practice-python/hackerrank/ctcic/array_left_rotation.py
+++
@@ -1,5 +0,0 @@
-def array_left_rotation(arr, n, k):
-  return arr[k:] + arr[:k]
-
-def array_right_rotation(arr, n, k):
-  return arr[-k:] + arr[:-k]
--- practice-python/learning-implementations/test/bst/node.py
+++
@@ -1,7 +0,0 @@
-class Node(object):
-  """docstring for Node"""
-  def __init__(self, data=None):
-    self.data = data
-    self.right = None
-    self.left = None
-    
